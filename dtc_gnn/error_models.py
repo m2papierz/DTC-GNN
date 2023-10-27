1e0e42f3db7cd9cff748efa38a7af41906c459ae
m2papierz <m2papierz@gmail.com>
2023-10-27 15:27:52 +0200

    Adjust error models to MWPM decoder of rotated planar code

--- dtc_gnn/error_models.py
+++ dtc_gnn/error_models.py
@@ -5,12 +5,12 @@ from typing import Union, Tuple, List
 from qecsim.paulitools import pauli_to_bsf
 
 
-class ErrorModelBase:
+class ErrorModel:
     paulis = ('I', 'X', 'Y', 'Z')
     rng = np.random.default_rng()
 
     @abc.abstractmethod
-    def prob_distribution(self, probability: float):
+    def probability_distribution(self, probability: float):
         pass
 
     def generate(
@@ -24,13 +24,13 @@ class ErrorModelBase:
         if isinstance(shape, tuple):
             pauli_errors = self.rng.choice(
                 self.paulis, size=shape,
-                p=self.prob_distribution(probability)
+                p=self.probability_distribution(probability)
             )
             pauli_errors = [''.join(e) for e in pauli_errors]
         else:
             pauli_errors = ''.join(self.rng.choice(
                 self.paulis, size=shape,
-                p=self.prob_distribution(probability)
+                p=self.probability_distribution(probability)
             ))
         return pauli_to_bsf(pauli_errors)
 
@@ -38,55 +38,55 @@ class ErrorModelBase:
         return '{}()'.format(type(self).__name__)
 
 
-class DepolarizingErrorModel(ErrorModelBase):
+class DepolarizingErrorModel(ErrorModel):
     """
     Implements a depolarizing error model.
 
     The probability distribution for a given error probability p is:
     (1 - p) for no error and p/3 for X, Y, Z errors.
     """
-    def prob_distribution(self, probability):
+    def probability_distribution(self, probability):
         p_x = p_y = p_z = probability / 3
         p_i = 1 - sum((p_x, p_y, p_z))
         return p_i, p_x, p_y, p_z
 
 
-class BitFlipErrorModel(ErrorModelBase):
+class BitFlipErrorModel(ErrorModel):
     """
     Implements a bit-flip error model.
 
     The probability distribution for a given error probability p is:
     (1 - p) for no error and p for X error.
     """
-    def prob_distribution(self, probability):
+    def probability_distribution(self, probability):
         p_x = probability
         p_y = p_z = 0
         p_i = 1 - sum((p_x, p_y, p_z))
         return p_i, p_x, p_y, p_z
 
 
-class PhaseFlipErrorModel(ErrorModelBase):
+class PhaseFlipErrorModel(ErrorModel):
     """
     Implements a phase-flip error model.
 
     The probability distribution for a given error probability p is:
     (1 - p) for no error and p for Z error.
     """
-    def prob_distribution(self, probability):
+    def probability_distribution(self, probability):
         p_x = p_y = 0
         p_z = probability
         p_i = 1 - sum((p_x, p_y, p_z))
         return p_i, p_x, p_y, p_z
 
 
-class BitPhaseFlipErrorModel(ErrorModelBase):
+class BitPhaseFlipErrorModel(ErrorModel):
     """
     Implements a bit-phase-flip error model.
 
     The probability distribution for a given error probability p is:
     (1 - p) for no error and p for Y error.
     """
-    def prob_distribution(self, probability):
+    def probability_distribution(self, probability):
         p_x = 0
         p_y = probability
         p_z = 0
